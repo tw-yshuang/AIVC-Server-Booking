8c646fc023a75d660ebf8f38b64b86250ab8f5b3
tw-yshuang <tw.yshuang@gmail.com>
2023-02-22 17:30:17 +0800

    [mod] image_setup/run_exec/entrypoint.sh, cannot rewrite $HISTFILE(~/.zsh_history) when it is volume.

--- image_setup/run_exec/entrypoint.sh
+++ image_setup/run_exec/entrypoint.sh
@@ -3,5 +3,6 @@
 # The command that needs to be executed when the container is running.
 
 bash /.script/fix_conflict.sh
+setopt nohistsavebycopy # cannot rewrite when volume $HISTFILE(~/.zsh_history).
 
 exec "$@"
