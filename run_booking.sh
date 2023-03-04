fecae2334b58f63127f77e0d8ca284209fa4c469
tw-yshuang <tw.yshuang@gmail.com>
2023-03-04 17:03:56 +0800

    [add] run_booking.sh, run booking image entry script.

---
+++ run_booking.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+
+#>         +---------------------------+
+#>         |    run_booking.sh    |
+#>         +---------------------------+
+#-
+#- SYNOPSIS
+#-
+#-    ./run_booking.sh [-h]
+#-    
+#-    It will run booking container with specific password for booking account.
+#-
+#- OPTIONS
+#-
+#-    -h, --help             print help information.
+#-
+#- EXAMPLES
+#-
+#-    $ ./run_booking.sh IamNo1handsome!
+
+#====================================================
+# Part 1. Option Tool
+#====================================================
+# Print script help
+function show_script_help(){
+    echo
+    head -22 $0 | # find this file top 16 lines.
+    grep "^#[-|>]" | # show the line that include "#-" or "#>".
+    sed -e "s/^#[-|>]*//1" # use nothing to replace "#-" or "#>" that the first keyword in every line.  
+    echo 
+}
+
+PROJ_PATH=/root
+password=$1
+if [ "$1" = "" ]; then
+    printf "Please enter a password for booking account: "
+    read password
+fi
+
+docker run \
+-dit \
+--cpus="0.5" \
+--restart=always \
+-p 10000:22 \
+--name=booking \
+-v ./cfg/:$PROJ_PATH/cfg \
+-v ./jobs/:$PROJ_PATH/jobs \
+-v ./lib/:$PROJ_PATH/lib \
+-v ./src/:$PROJ_PATH/src \
+-v $PROJ_PATH/src/monitor/ \
+rober5566a/aivc-server:booking-v1.0.0 \
+/bin/bash -c "/.script/ssh_start.sh $password"
